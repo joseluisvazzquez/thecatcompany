c7f6cbce1efda0471fa1b71c6a34dba40851fecb
joseluisvazzquez <jvazquez3@nigul.cide.es>
2024-03-14 17:09:19 +0100

    page-react

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,64 +1,74 @@
 import React, { useState } from 'react';
-import {
-  MDBNavbar,
-  MDBContainer,
-  MDBIcon,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBDropdown,
-  MDBDropdownMenu,
-  MDBDropdownToggle,
-  MDBDropdownItem,
-} from 'mdb-react-ui-kit';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+
 
 export default function Navbar() {
-  const [openNavCentred, setOpenNavCentred] = useState(false);
+  const [openNavNoTogglerSecond, setOpenNavNoTogglerSecond] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
-      <MDBContainer fluid>
-        <MDBNavbarToggler
-          type='button'
-          data-target='#navbarCenteredExample'
-          aria-controls='navbarCenteredExample'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setOpenNavCentred(!openNavCentred)}
+    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary py-4 shadow-5">
+      
+      <div className="container-fluid">
+       
+      <button
+      data-mdb-collapse-init
+      className="navbar-toggler"
+      type="button"
+      data-mdb-target="#navbarTogglerDemo01"
+      aria-controls="navbarTogglerDemo01"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars"></i>
+    </button>
+        <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+        </ul>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarCenteredExample"
         >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
+      
+          <ul className="navbar-nav mb-2 mb-lg-0 lead">
+           
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                data-mdb-dropdown-init
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
 
-        <MDBCollapse navbar open={openNavCentred} id='navbarCenteredExample'>
-          <MDBNavbarNav fullWidth={false} className='mb-2 mb-lg-0'>
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>Home</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-            </MDBNavbarItem>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">Action</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">Another action</a>
+                </li>
+                <li><hr className="dropdown-divider" /></li>
+                <li>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled"
+                >Disabled</a>
+            </li>
+          </ul>
 
-            <MDBNavbarItem>
-              <MDBDropdown>
-                <MDBDropdownToggle tag='a' className='nav-link'>Dropdown</MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem link>Action</MDBDropdownItem>
-                  <MDBDropdownItem link>Another action</MDBDropdownItem>
-                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                Disabled
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+        </div>
+       
+      </div>
+    </nav>
   );
 }
